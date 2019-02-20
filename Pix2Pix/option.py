24429666ec0294f5ba909c80e281c48770a2b733
Noel <36836739+NoelShin@users.noreply.github.com>
2019-02-21 00:12:36 +0900

    Update option.py

--- Pix2Pix/option.py
+++ Pix2Pix/option.py
@@ -1,70 +1,69 @@
 import os
 import argparse
-
+from utils import configure
 
 class BaseOption(object):
     def __init__(self):
         self.parser = argparse.ArgumentParser()
 
-        self.parser.add_argument('--debug', action='store_true', default=False)
+        self.parser.add_argument('--debug', action='store_true', default=True)
+        self.parser.add_argument('--gpu_id', type=str, default='3')
 
         self.parser.add_argument('--batch_size', type=int, default=1)
-        self.parser.add_argument('--checkpoint_dir', type=str, default='./checkpoint')
+        self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints')
         self.parser.add_argument('--color_space', type=str, default='HSV')
+        self.parser.add_argument('--dataset_dir', type=str, default='./dataset')
         self.parser.add_argument('--dataset_format', type=str, default='png')
-        self.parser.add_argument('--gpu_id', type=str, default='3')
-        self.parser.add_argument('--image_size', type=int, default=256)
-        self.parser.add_argument('--input_channel', type=int, default=1)
-        self.parser.add_argument('--output_channel', type=int, default=3)
-        self.parser.add_argument('--n_gf', type=int, default=64)
-        self.parser.add_argument('--n_df', type=int, default=64)
-        self.parser.add_argument('--n_workers', type=int, default=2)
-        self.parser.add_argument('--patch_size', type=int, default=70)
-
+        self.parser.add_argument('--dataset_name', type=str, default='cityscape')
+        self.parser.add_argument('--image_height', type=int, default=1024)
+        self.parser.add_argument('--input_channel', type=int, default=35)  # For cityscape dataset whose label numbers are 35.
+        self.parser.add_argument('--output_channel', type=int, default=3)  # RGB
+        self.parser.add_argument('--max_ch', type=int, default=2**9)
+        self.parser.add_argument('--n_df', type=int, default=64, help='# of channels for the first layer in D')
+        self.parser.add_argument('--n_gf', type=int, default=64, help='# of channels for the first layer in G')
+        self.parser.add_argument('--n_workers', type=int, default=2, help='# of threads for loading data')
+        self.parser.add_argument('--patch_size', type=int, default=70, help='effective receptive field of D')
 
     def parse(self):
-        self.opt = self.parser.parse_args()
+        opt = self.parser.parse_args()
+        model_name = 'Patch_size_{}'.format(str(opt.patch_size))
+        if opt.is_train:
+            opt.checkpoints_dir = os.path.join(opt.checkpoints_dir, opt.dataset_name, model_name)
+            os.makedirs(opt.checkpoints_dir) if not os.path.isdir(opt.checkpoints_dir) else None
+            opt.dataset_dir = os.path.join(opt.dataset_dir, opt.dataset_name)
+            assert os.path.isdir(opt.dataset_dir), print('There is no {} directory.'.format(opt.dataset_dir))
 
-        args = vars(self.opt)
+            log_path = os.path.join(opt.checkpoints_dir, 'Model', 'opt.txt')
 
-        if self.opt.is_train:
-            if not os.path.isdir(self.opt.checkpoint_dir):
-                os.mkdir(self.opt.checkpoint_dir)
+            if os.path.isfile(log_path) and not opt.debug:
+                permission = input(
+                    "{} log already exists. Do you really want to overwrite this log? Y/N. : ".format(
+                        model_name + '/opt'))
+                if permission == 'Y':
+                    pass
 
-            if self.opt.patch_size == 70:
-                self.opt.checkpoint_dir = os.path.join(self.opt.checkpoint_dir, 'Patch_size_70_' + str(self.opt.color_space))
+                else:
+                    raise NotImplementedError("Please check {}".format(log_path))
 
-                if not os.path.isdir(self.opt.checkpoint_dir):
-                    os.mkdir(self.opt.checkpoint_dir)
+            args = vars(opt)
+            with open(log_path, 'wt') as log:
+                log.write('-' * 50 + 'Options' + '-' * 50 + '\n')
+                print('-' * 50 + 'Options' + '-' * 50)
+                for k, v in sorted(args.items()):
+                    log.write('{}: {}\n'.format(str(k), str(v)))
+                    print("{}: {}".format(str(k), str(v)))
+                log.write('-' * 50 + 'End' + '-' * 50)
+                print('-' * 50 + 'End' + '-' * 50)
+                log.close()
 
-            else:
-                self.opt.checkpoint_dir = os.path.join(self.opt.checkpoint_dir, 'Patch_size_' + str(self.opt.patch_size))
+        else:
+            opt.test_image_dir = os.path.join(opt.results)
 
-                if not os.path.isdir(self.opt.checkpoint_dir):
-                    os.mkdir(self.opt.checkpoint_dir)
+        if opt.debug:
+            opt.report_freq = 1
+            opt.save_freq = 1
 
-        elif not self.opt.is_train:
-            if not os.path.isdir(self.opt.result_dir):
-                os.mkdir(self.opt.result_dir)
-
-            if self.opt.patch_size == 70:
-                self.opt.result_dir = os.path.join(self.opt.result_dir, str(self.opt.patch_size) + '_' + self.opt.color_space)
-
-            elif not self.opt.patch_size == 70:
-                self.opt.result_dir = os.path.join(self.opt.result_dir, str(self.opt.patch_size))
-
-            if not os.path.isdir(self.opt.result_dir):
-                os.mkdir(self.opt.result_dir)
-
-        if self.opt.debug:
-            self.opt.report_freq = 1
-            self.opt.save_freq = 1
-
-        print("-"*50, "Option start", "-"*50)
-        for k, v in sorted(args.items()):
-            print("{}: {}".format(str(k), str(v)))
-        print("-"*50, "End start", "-"*50)
-
+        self.opt = opt
         return self.opt
 
 
@@ -73,22 +72,21 @@ class TrainOption(BaseOption):
         super(TrainOption, self).__init__()
 
         self.parser.add_argument('--beta1', type=float, default=0.5)
-        self.parser.add_argument('--beta2', type=float, default=0.5)
-        self.parser.add_argument('--dataset_dir', type=str, default='./dataset/Claire/Train')
+        self.parser.add_argument('--beta2', type=float, default=0.999)
         self.parser.add_argument('--display_freq', type=int, default=500)
         self.parser.add_argument('--flip', action='store_true', default=True)
         self.parser.add_argument('--is_train', action='store_true', default=True)
         self.parser.add_argument('--L1_lambda', type=int, default=10)
         self.parser.add_argument('--lr', type=float, default=0.0002)
-        self.parser.add_argument('--n_epoch', type=int, default=400)
-        self.parser.add_argument('--report_freq', type=int, default=10)
-        self.parser.add_argument('--save_freq', type=int, default=50000)
+        self.parser.add_argument('--n_epoch', type=int, default=200)
+        self.parser.add_argument('--report_freq', type=int, default=5)
+        self.parser.add_argument('--save_freq', type=int, default=20)
         self.parser.add_argument('--shuffle', action='store_true', default=True)
 
 
 class TestOption(BaseOption):
     def __init__(self):
         super(TestOption, self).__init__()
-        self.parser.add_argument('--dataset_dir', type=str, default='./dataset/Claire/Test')
-        self.parser.add_argument('--result_dir', type=str, default='./dataset/Claire/Test/Result')
         self.parser.add_argument('--is_train', action='store_true', default=False)
+        self.parser.add_argument('--shuffle', action='store_true', default=False)
+
