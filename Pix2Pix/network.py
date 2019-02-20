5d36bd9919d0551b01cff7dc7b51bac82358d152
Noel <36836739+NoelShin@users.noreply.github.com>
2019-02-21 00:12:19 +0900

    Update network.py

--- Pix2Pix/network.py
+++ Pix2Pix/network.py
@@ -1,144 +1,113 @@
 import torch
+from math import log2
 from utils import *
+
 
 class Generator(nn.Module):
     def __init__(self, opt):
         super(Generator, self).__init__()
-        self.opt = opt
-        self.set_weight()
-        self.build_model()
+        act_down = nn.LeakyReLU(0.2, inplace=True)
+        act_up = nn.ReLU(inplace=True)
+        image_height = opt.image_height
+        input_ch = opt.input_ch
+        max_ch = opt.max_ch
+        n_downsample = int(log2(image_height))
+        n_gf = opt.n_gf
+        norm = nn.BatchNorm2d
+        output_ch = opt.output_ch
 
-    def set_weight(self):
-        input_channel = self.opt.input_channel  # 1
-        output_channel = self.opt.output_channel  # 3
-        n_gf = self.opt.n_gf  # 64
+        idx_max_ch = int(log2(max_ch // n_gf))
+        for i in range(n_downsample):
+            if i == 0:
+                down_block = [nn.Conv2d(input_ch, n_gf, kernel_size=4, padding=1, stride=2, bias=False)]
+                up_block = [act_up,
+                            nn.ConvTranspose2d(2 * n_gf, output_ch, kernel_size=4, padding=1, stride=2, bias=False),
+                            nn.Tanh()]
 
-        # Encoder network starts
-        self.first_conv = nn.Conv2d(input_channel, n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x64x128x128
-        self.conv_1 = nn.Conv2d(n_gf, 2 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x128x64x64
-        self.conv_2 = nn.Conv2d(2 * n_gf, 4 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x256x32x32
-        self.conv_3 = nn.Conv2d(4 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x16x16
-        self.conv_4 = nn.Conv2d(8 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x8x8
-        self.conv_5 = nn.Conv2d(8 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x4x4
-        self.conv_6 = nn.Conv2d(8 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x2x2
-        self.conv_7 = nn.Conv2d(8 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x1x1
+            elif 1 <= i <= idx_max_ch:
+                down_block = [act_down, nn.Conv2d(n_gf, 2 * n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                              norm(2 * n_gf)]
+                up_block = [act_up, nn.ConvTranspose2d(4 * n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                            norm(n_gf)]
 
-        # Encoder network ends and Decoder network start
-        self.dconv_7 = nn.ConvTranspose2d(8 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x2x2
-        self.dconv_6 = nn.ConvTranspose2d(2 * 8 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x4x4
-        self.dconv_5 = nn.ConvTranspose2d(2 * 8 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x8x8
-        self.dconv_4 = nn.ConvTranspose2d(2 * 8 * n_gf, 8 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x16x16
-        self.dconv_3 = nn.ConvTranspose2d(2 * 8 * n_gf, 4 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x256x32x32
-        self.dconv_2 = nn.ConvTranspose2d(2 * 4 * n_gf, 2 * n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x128x64x64
-        self.dconv_1 = nn.ConvTranspose2d(2 * 2 * n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False)  # 1x64x128x128
-        self.last_dconv = nn.ConvTranspose2d(n_gf, output_channel, kernel_size=4, padding=1, stride=2, bias=False)  # 1x3x128x128
+            elif idx_max_ch < i < n_downsample - 4:
+                down_block = [act_down, nn.Conv2d(n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                              norm(2 * n_gf)]
+                up_block = [act_up, nn.ConvTranspose2d(2 * n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                            norm(n_gf)]
 
-    def build_model(self):
-        self.first_block = block(self.conv_1, relu=False)    # 1x128x64x64
-        self.second_block = block(self.conv_2, relu=False)  # 1x256x32x32
-        self.third_block = block(self.conv_3, relu=False)  # 1x512x64x64
-        self.fourth_block = block(self.conv_4, relu=False)
-        self.fifth_block = block(self.conv_5, relu=False)
-        self.sixth_block = block(self.conv_6, relu=False)
-        self.seventh_block = block(self.conv_7, relu=False, normalization=False)
+            elif n_downsample - 4 <= i < n_downsample - 2:
+                down_block = [act_down, nn.Conv2d(n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                              norm(n_gf)]
+                up_block = [act_up, nn.ConvTranspose2d(2 * n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                            norm(n_gf), nn.Dropout2d(0.5, inplace=True)]
 
-        self.seventh_dblock = block(self.dconv_7, transpose=True, dropout=True)
-        self.sixth_dblock = block(self.dconv_6, transpose=True, dropout=True)
-        self.fifth_dblock = block(self.dconv_5, transpose=True, dropout=True)
-        self.fourth_dblock = block(self.dconv_4, transpose=True)
-        self.third_dblock = block(self.dconv_3, transpose=True)
-        self.second_dblock = block(self.dconv_2, transpose=True)
-        self.first_dblock = block(self.dconv_1, transpose=True)
+            else:
+                down_block = [act_down, nn.Conv2d(n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False)]
+                up_block = [act_up, nn.ConvTranspose2d(n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                            norm(n_gf), nn.Dropout2d(0.5, inplace=True)]
+
+            self.add_module('Down_block_{}'.format(i), nn.Sequential(*down_block))
+            self.add_module('Up_block_{}'.format(i), nn.Sequential(*up_block))
+            n_gf *= 2 if n_gf < max_ch else None
+
+        self.n_downsample = n_downsample
 
     def forward(self, x):
-        d0 = self.first_conv(x)  # 1x64x128x128
-        d1 = self.first_block(d0)  # 1x128x64x64
-        d2 = self.second_block(d1)  # 1x256x32x32
-        d3 = self.third_block(d2)  # 1x512x16x16
-        d4 = self.fourth_block(d3)  # 1x512x8x8
-        d5 = self.fifth_block(d4)  # 1x512x4x4
-        d6 = self.sixth_block(d5)  # 1z512x2x2
-        d7 = self.seventh_block(d6)  # 1x512x1x1
-        u7 = self.seventh_dblock(d7)  # 1x512x2x2
-        u6 = self.sixth_dblock(torch.cat([u7, d6], dim=1))  # 1x512x4x4
-        u5 = self.fifth_dblock(torch.cat([u6, d5], dim=1))  # 1x512x8x8
-        u4 = self.fourth_dblock(torch.cat([u5, d4], dim=1))  # 1x512x16x16
-        u3 = self.third_dblock(torch.cat([u4, d3], dim=1))  # 1x512x32x32
-        u2 = self.second_dblock(torch.cat([u3, d2], dim=1))  # 1x512x64x64
-        u1 = nn.ReLU()(self.first_dblock(torch.cat([u2, d1], dim=1)))  # 1x512x128x128
-        u0 = nn.Tanh()(self.last_dconv(u1))  # 1x512x256x256
-        return u0
+        layers = [x]
+        for i in range(self.n_downsample):
+            layers += [getattr(self, 'Down_block_{}'.format(i))(layers[-1])]
+        x = getattr(self, 'Up_block_{}'.format(self.n_downsample - 1))(layers[-1])
+        for i in range(self.n_downsample - 2, -1, -1):
+            x = getattr(self, 'Up_block_{}'.format(i))(torch.cat([x, layers[i]], dim=1))
+        return x
 
 
 class Discriminator(nn.Module):
     def __init__(self, opt):
         super(Discriminator, self).__init__()
-        self.opt = opt
-        self.set_weight()
-        self.build_model()
+        act = nn.LeakyReLU(0.2, inplace=True)
+        input_ch = opt.input_ch + opt.output_ch
+        n_df = opt.n_df
+        norm = nn.BatchNorm2d
+        patch_size = opt.patch_size
 
-    def set_weight(self):
-        input_channel = self.opt.output_channel
-        n_df = self.opt.n_df
+        if patch_size == 1:
+            model = [nn.Conv2d(input_ch, n_df, kernel_size=1, bias=False), act]
+            model += [nn.Conv2d(n_df, 2 * n_df, kernel_size=1, bias=False), norm(2 * n_df), act]
+            model += [nn.Conv2d(2 * n_df, 1, kernel_size=1, bias=False)]
 
-        if self.opt.patch_size == 1:
-            self.conv_1 = nn.Conv2d(input_channel, n_df, kernel_size=1, padding=0, stride=1, bias=False)
-            self.conv_2 = nn.Conv2d(n_df, 2*n_df, kernel_size=1, padding=0, stride=1, bias=False)
-            self.conv_3 = nn.Conv2d(2*n_df, 1, kernel_size=1, padding=0, stride=1, bias=False)
+        elif patch_size == 16:
+            model = [nn.Conv2d(input_ch, n_df, kernel_size=4, padding=1, stride=2, bias=False), act]
+            model += [nn.Conv2d(n_df, 2 * n_df, kernel_size=4, padding=1, stride=2, bias=False), norm(2 * n_df), act]
+            model += [nn.Conv2d(2 * n_df, 4 * n_df, kernel_size=4, padding=1, stride=1, bias=False), norm(4 * n_df),
+                      act]
+            model += [nn.Conv2d(4 * n_df, 1, kernel_size=4, padding=1, stride=1, bias=False)]
+
+        elif patch_size == 70:
+            model = [nn.Conv2d(input_ch, n_df, kernel_size=4, padding=1, stride=2, bias=False), act]
+            model += [nn.Conv2d(n_df, 2 * n_df, kernel_size=4, padding=1, stride=2, bias=False), norm(2 * n_df), act]
+            model += [nn.Conv2d(2 * n_df, 4 * n_df, kernel_size=4, padding=1, stride=2, bias=False), norm(4 * n_df),
+                      act]
+            model += [nn.Conv2d(4 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=1, bias=False), norm(8 * n_df),
+                      act]
+            model += [nn.Conv2d(8 * n_df, 1, kernel_size=4, padding=1, stride=1, bias=False)]
+
+        elif patch_size == 286:
+            model = [nn.Conv2d(input_ch, n_df, kernel_size=4, padding=1, stride=2, bias=False), act]
+            model += [nn.Conv2d(n_df, 2 * n_df, kernel_size=4, padding=1, stride=2, bias=False), norm(2 * n_df), act]
+            model += [nn.Conv2d(2 * n_df, 4 * n_df, kernel_size=4, padding=1, stride=2, bias=False), norm(4 * n_df),
+                      act]
+            model += [nn.Conv2d(4 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=2, bias=False), norm(8 * n_df),
+                      act]
+            model += [nn.Conv2d(8 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=1, bias=False), norm(8 * n_df)]
+            model += [nn.Conv2d(8 * n_df, 1, kernel_size=4, padding=1, stride=1, bias=False)]
 
         else:
-            self.conv_1 = nn.Conv2d(input_channel, n_df, kernel_size=4, padding=1, stride=2, bias=False)  # 1x64x128x128
-            if self.opt.patch_size == 16:
-                self.conv_2 = nn.Conv2d(n_df, 2*n_df, kernel_size=4, padding=1, stride=2, bias=False)
-                self.conv_3 = nn.Conv2d(2*n_df, 4*n_df, kernel_size=4, padding=1, stride=1, bias=False)
-                self.conv_4 = nn.Conv2d(4*n_df, 1, kernel_size=4, padding=1, stride=1, bias=False)
+            raise NotImplementedError("Invalid patch size {}. Please choose among [1, 16, 70, 286].".format(patch_size))
 
-            elif self.opt.patch_size == 70:
-                self.conv_2 = nn.Conv2d(n_df, 2 * n_df, kernel_size=4, padding=1, stride=2, bias=False)  # 1x128x64x64
-                self.conv_3 = nn.Conv2d(2 * n_df, 4 * n_df, kernel_size=4, padding=1, stride=2, bias=False)  # 1x256x32x32
-                self.conv_4 = nn.Conv2d(4 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=1, bias=False)  # 1x512x31x31
-                self.conv_5 = nn.Conv2d(8 * n_df, 1, kernel_size=4, padding=1, stride=1, bias=False)  # 1x512x30x30
-
-            elif self.opt.patch_size == 286:
-                self.conv_2 = nn.Conv2d(n_df, 2 * n_df, kernel_size=4, padding=1, stride=2, bias=False)  # 1x128x64x64
-                self.conv_3 = nn.Conv2d(2 * n_df, 4 * n_df, kernel_size=4, padding=1, stride=2, bias=False)  # 1x256x32x32
-                self.conv_4 = nn.Conv2d(4 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x31x31
-                self.conv_5 = nn.Conv2d(8 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=2, bias=False)  # 1x512x30x30
-                self.conv_6 = nn.Conv2d(8 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=1, bias=False)
-                self.conv_7 = nn.Conv2d(8 * n_df, 1, kernel_size=4, padding=1, stride=1, bias=False)
-
-    def build_model(self):
-        self.model = nn.Sequential()
-
-        self.model.add_module('first_conv', self.conv_1)
-        if self.opt.patch_size == 1:
-            self.model.add_module('first_block', block(self.conv_2, relu=False))
-            self.model.add_module('last_leaky', nn.LeakyReLU(0.2, inplace=True))
-            self.model.add_module('last_conv', self.conv_3)
-
-        elif self.opt.patch_size == 16:
-            self.model.add_module('first_block', block(self.conv_2, relu=False))
-            self.model.add_module('second_block', block(self.conv_3, relu=False))
-            self.model.add_module('last_leaky', nn.LeakyReLU(0.2, inplace=True))
-            self.model.add_module('last_conv', self.conv_4)
-
-        elif self.opt.patch_size == 70:
-            self.model.add_module('first_block', block(self.conv_2, relu=False))
-            self.model.add_module('second_block', block(self.conv_3, relu=False))
-            self.model.add_module('third_block', block(self.conv_4, relu=False))
-            self.model.add_module('last_leaky', nn.LeakyReLU(0.2, inplace=True))
-            self.model.add_module('last_conv', self.conv_5)
-
-        elif self.opt.patch_size == 286:
-            self.model.add_module('first_block', block(self.conv_2, relu=False))
-            self.model.add_module('second_block', block(self.conv_3, relu=False))
-            self.model.add_module('third_block', block(self.conv_4, relu=False))
-            self.model.add_module('fourth_block', block(self.conv_5, relu=False))
-            self.model.add_module('fifth_block', block(self.conv_6, relu=False))
-            self.model.add_module('last_leaky', nn.LeakyReLU(0.2, inplace=True))
-            self.model.add_module('last_conv', self.conv_7)
-
-        self.model.add_module('sigmoid', nn.Sigmoid())
+        model += [nn.Sigmoid()]
+        self.add_module('Model', nn.Sequential(*model))
 
     def forward(self, x):
-        return self.model(x)
+        return getattr(self, 'Model')(x)
