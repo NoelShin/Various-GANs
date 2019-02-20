67d21775a1e50c704f633b9c71a6a0c881d4b79e
Noel <36836739+NoelShin@users.noreply.github.com>
2019-02-21 00:12:50 +0900

    Update pipeline.py

--- Pix2Pix/pipeline.py
+++ Pix2Pix/pipeline.py
@@ -15,13 +15,11 @@ class CustomDataset(torch.utils.data.Dataset):
         if self.opt.is_train:
             self.target_path_list = sorted(glob.glob(os.path.join(self.opt.dataset_dir, 'Target', '*.' + self.opt.dataset_format)))
 
-    def get_transform(self, normalize=True):
+    def get_transform(self, normalize=False):
         transform_list = []
 
-        if self.opt.is_train and self.opt.flip:
-            coin = random.random() > 0.5
-            if coin:
-                transform_list.append(transforms.Lambda(lambda x: self.__flip(x)))
+        if self.coin:
+            transform_list.append(transforms.Lambda(lambda x: self.__flip(x)))
 
         transform_list.append(transforms.ToTensor())
 
@@ -35,16 +33,15 @@ class CustomDataset(torch.utils.data.Dataset):
         return x.transpose(Image.FLIP_LEFT_RIGHT)
 
     def __getitem__(self, index):
-        transform = self.get_transform()
+        if self.opt.is_train and self.opt.flip:
+            self.coin = random.random() > 0.5
 
-        input_path = self.input_path_list[index]
-        input_array = Image.open(input_path)
-        input_tensor = transform(input_array)
+        input_image = Image.open(self.input_path_list[index])
+        input_tensor = self.get_transform()(input_image)
 
         if self.opt.is_train:
-            target_path = self.target_path_list[index]
-            target_array = Image.open(target_path).convert(self.opt.color_space)
-            target_tensor = transform(target_array)
+            target_image = Image.open(self.target_path_list[index])
+            target_tensor = self.get_transform(normalize=True)(target_image)
 
             return input_tensor, target_tensor
 
