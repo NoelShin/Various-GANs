e163983bc142c0c7caa5659e578a7bbddac40bdd
Noel <36836739+NoelShin@users.noreply.github.com>
2020-01-24 16:04:58 +0900

    Update options.py

--- CycleGAN/options.py
+++ CycleGAN/options.py
@@ -5,7 +5,7 @@ import argparse
 class BaseOption(object):
     def __init__(self):
         self.args = argparse.ArgumentParser()
-        self.args.add_argument('--debug', action='store_true', default=True)
+        self.args.add_argument('--debug', action='store_true', default=False)
         self.args.add_argument('--gpu_id', type=str, default='0', help='-1 for using CPU.')
 
         self.args.add_argument('--batch_size', type=int, default=1)
@@ -13,9 +13,9 @@ class BaseOption(object):
         self.args.add_argument('--dir_checkpoints', type=str, default='./checkpoints')
         self.args.add_argument('--dir_datasets', type=str, default='./datasets')
         self.args.add_argument('--in_channels', type=int, default=3,
-                               help='Number of input channels in the generator.')
+                               help='Number of input channels to the generator.')
         self.args.add_argument('--out_channels', type=int, default=3,
-                               help='Number of output channels in the generator.')
+                               help='Number of output channels from the generator.')
         self.args.add_argument('--n_df', type=int, default=64,
                                help='Nb of output channels of the first layer in the discriminator.')
         self.args.add_argument('--n_gf', type=int, default=32,
@@ -27,7 +27,7 @@ class BaseOption(object):
         args = self.args.parse_args()
         args.dir_image_train = os.path.join(args.dir_checkpoints, args.dataset_name, 'Train', 'Image')
         args.dir_image_test = os.path.join(args.dir_checkpoints, args.dataset_name, 'Test', 'Image')
-        args.dir_model = os.path.join(args.dir_checkpoints, args.dataset_name, 'Train', 'Model')
+        args.dir_model = os.path.join(args.dir_checkpoints, args.dataset_name, 'Model')
         os.makedirs(args.dir_image_train, exist_ok=True)
         os.makedirs(args.dir_image_test, exist_ok=True)
         os.makedirs(args.dir_model, exist_ok=True)
@@ -47,7 +47,7 @@ class BaseOption(object):
 class TrainOption(BaseOption):
     def __init__(self):
         super(TrainOption, self).__init__()
-        self.args.add_argument('--is_train', action='store_true', default=True)
+        self.args.add_argument('--is_train', type=bool, default=True)
 
         self.args.add_argument('--beta_1', type=float, default=0.5, help='Adam optimizer param.')
         self.args.add_argument('--beta_2', type=float, default=0.999, help='Adam optimizer param.')
@@ -70,4 +70,4 @@ class TrainOption(BaseOption):
 class TestOption(BaseOption):
     def __init__(self):
         super(TestOption, self).__init__()
-        self.args.add_argument('--is_train', action='store_true', default=False)
+        self.args.add_argument('--is_train', type=bool, default=False)
